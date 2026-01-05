414f6aafbc1520eb0125dafa0dc1aa14c66ec397
Jayashree Hedaoo <Jayashree.Hedaoo+cba@cba.com.au>
2026-01-05 13:10:23 +0530

    refactor: remove modal prop from RecipesLayout and simplify return structure

--- src/app/recipes/layout.tsx
+++ src/app/recipes/layout.tsx
@@ -1,14 +1,7 @@
 export default function RecipesLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
-  modal: React.ReactNode;
 }) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  );
+  return <>{children}</>;
 }
